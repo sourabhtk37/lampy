1e15237d77df3e23c8cb92ec4841e915e87ea468
seht <seht@users.noreply.github.com>
2015-09-26 16:11:20 +0100

    major redesign, implements GtkBuilder/glade

--- gui/main.py
+++ gui/main.py
@@ -1,164 +1,148 @@
-# @todo porting the gui to Glade and Gtk.Builder
-
 from gi.repository import Gtk
 from collections import OrderedDict
 
 from glampy.common.sites import Sites
-from glampy.gui.conf import Conf
 
 
-class Main(Gtk.Window):
+# @todo separate events class
+
+class Main(object):
     def __init__(self):
-        super(Main, self).__init__()
 
-        self.gtk_layout = Gtk.VBox()
-        self.gtk_system = Gtk.VBox()
-        self.gtk_templates = Gtk.VBox()
-        self.gtk_tabs = Gtk.Notebook()
-        self.gtk_main_controls = Gtk.HButtonBox()
+        self.gtk_builder = Gtk.Builder()
+
+        self.ui_layout = self.gtk_builder.new_from_file("gui/glade/main_window.glade")
+        self.ui_sites_list_container = self.gtk_builder.new_from_file("gui/glade/sites_list_container.glade")
+        self.ui_sites_edit_container = self.gtk_builder.new_from_file("gui/glade/sites_edit_container.glade")
+
+        self.gtk_main_window = self.ui_layout.get_object("main_window")
+
+        self.gtk_sites_list_view = self.ui_layout.get_object("sites_list_view")
+        self.gtk_sites_edit_view = self.ui_layout.get_object("sites_edit_view")
+
+        self.gtk_sites_list_view.add(self.ui_sites_list_container.get_object("sites_list_container"))
+        self.gtk_sites_edit_view.add(self.ui_sites_edit_container.get_object("sites_edit_container"))
+
+        self.gtk_sites_list_container = self.ui_sites_list_container.get_object("sites_list_container")
+        self.gtk_sites_edit_container = self.ui_sites_edit_container.get_object("sites_edit_container")
+        self.gtk_sites_conf_container = self.ui_sites_edit_container.get_object("sites_conf_container")
+
+        self.gtk_sites_list_nav = self.ui_sites_list_container.get_object("sites_list_nav")
+
+        self.sites_list_nav_activated = None
 
         self.sites = Sites().read()
 
-        self.gtk_sites_table = {}
+        # gtk_edit_button_undo = Gtk.Button(stock=Gtk.STOCK_UNDO)
+        gtk_edit_button_remove = Gtk.Button(stock=Gtk.STOCK_REMOVE)
+        gtk_edit_button_add = Gtk.Button(stock=Gtk.STOCK_ADD)
+        gtk_edit_button_save = Gtk.Button(stock=Gtk.STOCK_SAVE)
 
-        self.build_main_controls()
-        self.build_sites_table()
-        self.build_layout()
-        self.build_system()
-        self.build_tabs()
+        gtk_list_button_remove = Gtk.Button(stock=Gtk.STOCK_REMOVE)
+        gtk_list_button_add = Gtk.Button(stock=Gtk.STOCK_ADD)
+        gtk_list_button_save = Gtk.Button(stock=Gtk.STOCK_SAVE)
 
-        self.add(self.gtk_tabs)
+        gtk_edit_button_remove.connect('clicked', self.on_edit_remove_clicked)
+        gtk_edit_button_add.connect('clicked', self.on_edit_add_clicked)
+        gtk_edit_button_save.connect('clicked', self.on_edit_save_clicked)
 
-        self.set_title('Glampy')
-        self.set_position(Gtk.WindowPosition.CENTER)
-        self.connect('destroy', Gtk.main_quit)
+        gtk_list_button_remove.connect('clicked', self.on_list_remove_clicked)
+        gtk_list_button_add.connect('clicked', self.on_list_add_clicked)
+        gtk_list_button_save.connect('clicked', self.on_list_save_clicked)
 
-        self.show_all()
-        self.resize(1, 1)
+        self.gtk_edit_action_bar = Gtk.ActionBar()
+        self.gtk_edit_action_bar.add(gtk_edit_button_remove)
+        self.gtk_edit_action_bar.add(gtk_edit_button_add)
+        self.gtk_edit_action_bar.add(gtk_edit_button_save)
+
+        self.gtk_list_action_bar = Gtk.ActionBar()
+        self.gtk_list_action_bar.add(gtk_list_button_remove)
+        self.gtk_list_action_bar.add(gtk_list_button_add)
+        self.gtk_list_action_bar.add(gtk_list_button_save)
+
+        self.gtk_sites_edit_container.add(self.gtk_edit_action_bar)
+        self.gtk_sites_list_container.add(self.gtk_list_action_bar)
+
+        self.gtk_conf_model = Gtk.ListStore(str, str)
+        self.gtk_conf_tree = self.ui_sites_edit_container.get_object("sites_conf_tree")
+
+        self.build_list_nav()
+        self.build_conf_tree()
+
+        self.gtk_main_window.show_all()
+
+        self.gtk_sites_edit_container.hide()
 
         Gtk.main()
 
-    def build_layout(self):
-        self.gtk_layout.add(self.gtk_main_controls)
-        self.gtk_layout.add(self.gtk_sites_table['container'])
-
-    def build_system(self):
-        pass
-
-    def build_tabs(self):
-        sites_tab = Gtk.Label()
-        system_tab = Gtk.Label()
-        templates_tab = Gtk.Label()
-
-        sites_tab.set_text('Sites')
-        system_tab.set_text('System')
-        templates_tab.set_text('Templates')
-
-        self.gtk_tabs.append_page(self.gtk_layout, sites_tab)
-        self.gtk_tabs.append_page(self.gtk_system, system_tab)
-        self.gtk_tabs.append_page(self.gtk_templates, templates_tab)
-
-    def build_main_controls(self):
-        add_control = Gtk.Button(stock=Gtk.STOCK_ADD)
-        reset_control = Gtk.Button(stock=Gtk.STOCK_UNDO)
-        save_control = Gtk.Button(stock=Gtk.STOCK_SAVE)
-
-        add_control.connect('clicked', self.on_add_clicked)
-        reset_control.connect('clicked', self.on_reset_clicked)
-        save_control.connect('clicked', self.on_save_clicked)
-
-        self.gtk_main_controls.add(add_control)
-        self.gtk_main_controls.add(reset_control)
-        self.gtk_main_controls.add(save_control)
-
-    def build_sites_table(self):
-        self.gtk_sites_table = {
-            'container': Gtk.VBox(),
-            'children': [],
-            'controls': OrderedDict([
-                ('remove', []),
-                ('server', []),
-                ('folder', []),
-                ('config', []),
-                ('enable', []),
-            ])
-        }
+    def build_list_nav(self):
         for site in self.sites:
-            self.sites_table_add(site)
+            row = Gtk.ListBoxRow()
+            if 'ServerName' in site:
+                row.add(Gtk.Label(site['ServerName']))
+            self.gtk_sites_list_nav.add(row)
+        self.gtk_sites_list_nav.connect('row-activated', self.on_list_nav_activated)
 
-    def reset_sites_table(self):
-        self.sites = Sites().read()
-        self.gtk_sites_table['container'].destroy()
-        self.build_sites_table()
-        self.gtk_layout.add(self.gtk_sites_table['container'])
-        self.resize(1, 1)
-
-    def sites_table_add(self, directives={}):
-        controls = {
-            'server': Gtk.Entry(),
-            'folder': Gtk.FileChooserButton('Folder'),
-            'config': Gtk.Button(stock=Gtk.STOCK_PROPERTIES),
-            'enable': Gtk.Switch(),
-            'remove': Gtk.Button(stock=Gtk.STOCK_REMOVE),
-        }
-        controls['folder'].set_action(Gtk.FileChooserAction.SELECT_FOLDER)
-
-        if 'ServerName' in directives and directives['ServerName']:
-            controls['server'].set_text(directives['ServerName'])
-        if 'DocumentRoot' in directives and directives['DocumentRoot']:
-            controls['folder'].set_current_folder(directives['DocumentRoot'])
-
-        controls['config'].connect('clicked', self.on_config_clicked)
-        controls['remove'].connect('clicked', self.on_remove_clicked)
-        # controls['enable'].connect('clicked', self.on_enable_clicked)
-
-        child = Gtk.HBox()
-
-        for control in self.gtk_sites_table['controls']:
-            self.gtk_sites_table['controls'][control].append(controls[control])
-            child.add(self.gtk_sites_table['controls'][control][-1])
-
-        self.gtk_sites_table['children'].append(child)
-
-        self.gtk_sites_table['container'].add(self.gtk_sites_table['children'][-1])
-        self.gtk_sites_table['container'].show_all()
-
-    def sites_table_remove(self, index):
-        self.gtk_sites_table['container'].remove(self.gtk_sites_table['children'][index])
-        self.gtk_sites_table['children'][index].destroy()
-        self.gtk_sites_table['container'].check_resize()
-        self.resize(1, 1)
-
-        for control in self.gtk_sites_table['controls']:
-            del self.gtk_sites_table['controls'][control][index]
-        del self.gtk_sites_table['children'][index]
-
-    def on_config_clicked(self, button):
-        index = self.gtk_sites_table['controls']['config'].index(button)
-        server = self.gtk_sites_table['controls']['server'][index].get_text()
-        folder = self.gtk_sites_table['controls']['folder'][index].get_current_folder()
-        self.sites[index].update({'ServerName': server, 'DocumentRoot': folder})
-        Conf(self, index)
-
-    def on_enable_clicked(self, button):
+    def rebuild_list_nav(self):
         pass
 
-    def on_remove_clicked(self, button):
-        index = self.gtk_sites_table['controls']['remove'].index(button)
-        self.sites.pop(index)
-        self.sites_table_remove(index)
+    def build_conf_tree(self):
+        self.gtk_conf_tree.set_model(self.gtk_conf_model)
+        self.gtk_conf_tree.set_reorderable(True)
+        cols = ['Directive', 'Value']
+        for index, title in enumerate(cols):
+            cell = Gtk.CellRendererText()
+            cell.set_property('editable', True)
+            cell.connect('edited', self.on_edit_cell_edited, index)
+            col = Gtk.TreeViewColumn(title, cell)
+            col.add_attribute(cell, 'text', index)
+            self.gtk_conf_tree.append_column(col)
 
-    def on_add_clicked(self, button):
-        self.sites.append({'ServerName': '', 'DocumentRoot': ''})
-        self.sites_table_add()
+    def load_conf_tree(self, id):
+        self.gtk_conf_model.clear()
+        for name, value in self.sites[id].iteritems():
+            self.conf_tree_add(name, value)
 
-    def on_reset_clicked(self, button):
-        self.reset_sites_table()
+    def conf_tree_add(self, name=None, value=None):
+        self.gtk_conf_model.append([name, value])
+        self.gtk_conf_tree.show_all()
 
-    def on_save_clicked(self, button):
-        for index, site in enumerate(self.sites):
-            server = self.gtk_sites_table['controls']['server'][index].get_text()
-            folder = self.gtk_sites_table['controls']['folder'][index].get_current_folder()
-            site.update({'DocumentRoot': folder, 'ServerName': server})
+    def on_edit_add_clicked(self, button):
+        self.conf_tree_add('', '')
+
+    def on_edit_remove_clicked(self, button):
+        list_store, tree_iter = self.gtk_conf_tree.get_selection().get_selected()
+        if tree_iter:
+            list_store.remove(tree_iter)
+        self.gtk_main_window.resize(1, 1)
+
+    def on_edit_save_clicked(self, button):
+        directives = OrderedDict()
+        for directive in self.gtk_conf_model:
+            directives.update({directive[0]: directive[1]})
+        self.sites[self.sites_list_nav_activated] = directives
         Sites().write(self.sites)
-        self.reset_sites_table()
+        self.rebuild_list_nav()
+
+    def on_edit_cell_edited(self, cell, path, text, col):
+        self.gtk_conf_model[path][col] = text
+
+    def on_list_nav_activated(self, box, row, user_data=None):
+        self.sites_list_nav_activated = row.get_index()
+        self.load_conf_tree(self.sites_list_nav_activated)
+        self.gtk_sites_edit_container.show()
+
+    def on_list_add_clicked(self, button):
+        self.sites.append({'ServerName': 'server.local', 'DocumentRoot': ''})
+        row = Gtk.ListBoxRow()
+        row.add(Gtk.Label('server.local'))
+        self.gtk_sites_list_nav.add(row)
+        self.gtk_sites_list_nav.show_all()
+
+    def on_list_remove_clicked(self, button):
+        self.sites.pop(self.sites_list_nav_activated)
+        self.gtk_sites_list_nav.remove(self.gtk_sites_list_nav.get_selected_row())
+        self.gtk_main_window.resize(1, 1)
+
+    def on_list_save_clicked(self, button):
+        Sites().write(self.sites)
