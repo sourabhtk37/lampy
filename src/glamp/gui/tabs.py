1aa1bc4791e7d0c5ac90ec7491ab8a771022d9c0
seht <seht@users.noreply.github.com>
2015-10-12 21:01:42 +0100

    some loving...

--- src/glamp/gui/tabs.py
+++ src/glamp/gui/tabs.py
@@ -3,27 +3,29 @@ from collections import OrderedDict
 from glamp.lib.storage import Sites
 
 class SitesTab(Gtk.Box):
-     
+    
+    SPACING = 10
+    PADDING = 5
+    
     def __new__(cls, main_window):
-        sites_tab_container = main_window.builder.get_object("sites_tab_container")
-        sites_tab_container.__class__ = cls
-        return sites_tab_container
+        box = main_window.builder.get_object("sites_tab_container")
+        box.__class__ = cls
+        return box
     
     def __init__(self, main_window):
         self.main_window = main_window
         self.storage = Sites()
         self.sites = self.storage.read()
+        self.list_active = None
 
-        self.sites_list_view = self.main_window.builder.get_object("sites_list_view")
-        self.sites_edit_view = self.main_window.builder.get_object("sites_edit_view")
+        self.list_view = self.main_window.builder.get_object("sites_list_view")
+        self.edit_view = self.main_window.builder.get_object("sites_edit_view")
 
-        self.sites_list_container = self.main_window.builder.get_object("sites_list_container")
-        self.sites_edit_container = self.main_window.builder.get_object("sites_edit_container")
-        self.sites_conf_container = self.main_window.builder.get_object("sites_conf_container")
+        self.list_container = self.main_window.builder.get_object("sites_list_container")
+        self.edit_container = self.main_window.builder.get_object("sites_edit_container")
+        self.conf_container = self.main_window.builder.get_object("sites_conf_container")
 
-        self.sites_list_nav = self.main_window.builder.get_object("sites_list_nav")
-        self.sites_list_nav.connect('row-activated', self.on_list_nav_activated)
-
+        self.list_nav = self.main_window.builder.get_object("sites_list_nav")
         self.conf_tree = self.main_window.builder.get_object("sites_conf_tree")
         self.conf_model = Gtk.ListStore(str, str)
         
@@ -31,61 +33,48 @@ class SitesTab(Gtk.Box):
         self.edit_address_input = self.main_window.builder.get_object("address_input")
         self.edit_docroot_input = self.main_window.builder.get_object("docroot_input")
 
-        self.edit_action_bar = Gtk.ActionBar()
-        self.list_action_bar = Gtk.ActionBar()
-
         self.build_list_nav()
         self.build_conf_tree()
         self.build_list_action_bar()
         self.build_edit_action_bar()
 
     def build_list_action_bar(self):
-        list_button_remove = Gtk.Button(stock=Gtk.STOCK_REMOVE)
-        list_button_add = Gtk.Button(stock=Gtk.STOCK_ADD)
-        list_button_save = Gtk.Button(stock=Gtk.STOCK_SAVE)
+        list_remove_button = self.main_window.builder.get_object("sites_list_remove_button")
+        list_add_button = self.main_window.builder.get_object("sites_list_add_button")
+        list_save_button = self.main_window.builder.get_object("sites_list_save_button")
 
-        list_button_remove.connect('clicked', self.on_list_remove_clicked)
-        list_button_add.connect('clicked', self.on_list_add_clicked)
-        list_button_save.connect('clicked', self.on_list_save_clicked)
-
-        self.list_action_bar.add(list_button_remove)
-        self.list_action_bar.add(list_button_add)
-        self.list_action_bar.add(list_button_save)
-        self.sites_list_container.add(self.list_action_bar)
+        list_remove_button.connect('clicked', self.on_list_remove_clicked)
+        list_add_button.connect('clicked', self.on_list_add_clicked)
+        list_save_button.connect('clicked', self.on_list_save_clicked)
 
     def build_edit_action_bar(self):
-        edit_button_remove = Gtk.Button(stock=Gtk.STOCK_REMOVE)
-        edit_button_add = Gtk.Button(stock=Gtk.STOCK_ADD)
-        edit_button_save = Gtk.Button(stock=Gtk.STOCK_SAVE)
+        edit_remove_button = self.main_window.builder.get_object("sites_edit_remove_button")
+        edit_add_button = self.main_window.builder.get_object("sites_edit_add_button")
+        edit_save_button = self.main_window.builder.get_object("sites_edit_save_button")
 
-        edit_button_remove.connect('clicked', self.on_edit_remove_clicked)
-        edit_button_add.connect('clicked', self.on_edit_add_clicked)
-        edit_button_save.connect('clicked', self.on_edit_save_clicked)
-
-        self.edit_action_bar.add(edit_button_remove)
-        self.edit_action_bar.add(edit_button_add)
-        self.edit_action_bar.add(edit_button_save)
-        self.sites_edit_container.add(self.edit_action_bar)
+        edit_remove_button.connect('clicked', self.on_edit_remove_clicked)
+        edit_add_button.connect('clicked', self.on_edit_add_clicked)
+        edit_save_button.connect('clicked', self.on_edit_save_clicked)
 
     def build_list_nav(self):
         for site in self.sites:
             self.list_nav_add(site)
+        self.list_nav.connect('row-activated', self.on_list_nav_activated)
 
     def rebuild_list_nav(self):
         pass
 
     def list_nav_add(self, site):
         row = Gtk.ListBoxRow()
-        col = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        col.add(Gtk.Label(site['hostname']))
-        col.add(Gtk.Switch())
+        col = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, homogeneous=True, spacing=0)
+        col.add(Gtk.Label(site['hostname'], margin=self.SPACING, margin_right=self.PADDING))
+        col.add(Gtk.Switch(margin=self.SPACING, margin_left=self.PADDING))
         row.add(col)
-        self.sites_list_nav.add(row)
-        self.sites_list_nav.show_all()
+        self.list_nav.add(row)
+        self.list_nav.show_all()
 
     def build_conf_tree(self):
         self.conf_tree.set_model(self.conf_model)
-        self.conf_tree.set_reorderable(True)
         for index, title in enumerate(['Property', 'Argument']):
             cell = Gtk.CellRendererText(editable=True)
             cell.connect('edited', self.on_edit_cell_edited, index)
@@ -99,6 +88,7 @@ class SitesTab(Gtk.Box):
         self.conf_model.clear()
         for property, argument in self.sites[id]['directives'].iteritems():
             self.conf_tree_add(property, argument)
+        self.main_window.resize(1, 1)
 
     def conf_tree_add(self, property=None, argument=None):
         self.conf_model.append([property, argument])
@@ -111,10 +101,10 @@ class SitesTab(Gtk.Box):
         list_store, tree_iter = self.conf_tree.get_selection().get_selected()
         if tree_iter:
             list_store.remove(tree_iter)
-        self.main_window.resize(1, 1)
+            self.main_window.resize(1, 1)
 
     def on_edit_save_clicked(self, button):
-        id = self.sites_list_nav.get_selected_row().get_index()
+        id = self.list_active.get_index()
         directives = OrderedDict()
         for directive in self.conf_model:
             directives.update({directive[0]: directive[1]})
@@ -129,24 +119,26 @@ class SitesTab(Gtk.Box):
         self.conf_model[path][col] = text
 
     def on_list_nav_activated(self, box, row, user_data=None):
-        self.load_conf_tree(row.get_index())
-        self.sites_edit_view.show()
+        self.list_active = row
+        self.load_conf_tree(self.list_active.get_index())
+        self.edit_view.show()
 
     def on_list_add_clicked(self, button):
-        site = {
+        new = {
             'hostname': 'virtual.localhost',
             'address': '127.0.0.1',
             'docroot': '',
             'directives': {},
         }
-        self.sites.append(site)
-        self.list_nav_add(site)
+        self.sites.append(new)
+        self.list_nav_add(new)
 
     def on_list_remove_clicked(self, button):
-        row = self.sites_list_nav.get_selected_row()
-        self.sites.pop(row.get_index())
-        self.sites_list_nav.remove(row)
-        self.main_window.resize(1, 1)
+        if self.list_active:
+            self.sites.pop(self.list_active.get_index())
+            self.list_nav.remove(self.list_active)
+            self.list_active = None
+            self.main_window.resize(1, 1)
 
     def on_list_save_clicked(self, button):
         self.storage.write(self.sites)
--- src/glamp/gui/window.py
+++ src/glamp/gui/window.py
@@ -6,9 +6,9 @@ class MainWindow(Gtk.ApplicationWindow):
      
     def __new__(cls, application):
         application.builder.add_from_file("glamp/gui/ui/main_window.xml")
-        main_window = application.builder.get_object("main_window")
-        main_window.__class__ = cls
-        return main_window
+        window = application.builder.get_object("main_window")
+        window.__class__ = cls
+        return window
     
     def __init__(self, application):
         # super(MainWindow, self).__init__()
@@ -18,4 +18,4 @@ class MainWindow(Gtk.ApplicationWindow):
         self.sites_tab_container = SitesTab(self)
 
         self.show_all()
-        self.sites_tab_container.sites_edit_view.hide()
+        self.sites_tab_container.edit_view.hide()
